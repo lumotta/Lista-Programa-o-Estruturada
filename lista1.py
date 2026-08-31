b1f4b7e16a127a5cee64b8ce0bc76fc9b51f1038
villalva <felipevilmotta@gmail.com>
2026-08-31 11:41:46 -0300

    Lista 1

---
+++ lista1.py
@@ -0,0 +1,122 @@
+# Exer 1
+# Escreva um programa em python que imprima a mensagem: 
+# “É PRECISO FAZER TODOS OS ALGORITMOS PARA APRENDER”.
+
+print("É PRECISO FAZER TODOS OS ALGORITMOS PARA APRENDER.")
+
+
+
+#Exer 2
+# Escreva um programa em python que leia um tempo em segundos 
+# e imprima quantas horas, minutos e segundos há neste tempo.
+
+s = float(input("Digite um valor em segundos: "))
+
+m = s / 60
+h = m / 60
+
+print(f"{h} h | {m} min | {s} s")
+
+
+
+# Exer 3
+# 3) Escreva um programa em python que leia um comprimento 
+# em centímetros e imprima quantos metros, decímetros e 
+# centímetros há neste comprimento.
+
+cm = float(input("Digite um valor em centímetros: "))
+
+dm = cm / 10
+m = dm / 10
+
+print(f"{m} m | {dm} dm | {cm} cm")
+
+
+
+# Exer 4
+# 4) Escreva um programa em python que leia a idade de 
+# uma pessoa expressa em anos, meses e dias e escreva a i
+# dade dessa pessoa expressa apenas em dias. Considerar ano
+# com 365 dias e mês com 30 dias.
+
+idade = int(input("Digite sua idade: "))
+
+mes = idade * 12
+dia = idade * 365
+
+print(f"Sua idade em dias: {dia} dias") 
+
+
+
+# Exer 5
+# Escreva um programa em python que entre com dois números 
+# reais e imprima a média aritmética com a mensagem “média” 
+# antes do resultado.
+
+x = float(input("Digite primeiro número real: "))
+y = float(input("Digite segundo número real: "))
+
+media = (x + y)/2
+
+print(f"Média: {media}")
+
+# Exer 6
+# Escreva um programa em python que possa entrar com o saldo 
+# de uma aplicação e imprimir o novo saldo, considerando o 
+# reajuste de 1%.
+
+saldo = float(input("Digite seu saldo para o reajuste de 1%: "))
+
+reaj = saldo * 1.01
+
+print(f"Seu reajuste foi {reaj}")
+
+# Exer 7
+# Escreva um programa em python que leia número no formato CDU 
+# e imprimir invertido: UDC (Exemplo: 123,saíra 321).
+
+digito = int(input("Digite um numero para inverter: "))
+
+unidade = digito % 10
+resto = digito // 10
+
+dezena = resto % 10
+resto = resto // 10
+
+centena = resto % 10
+resto = resto // 10
+
+total = (unidade * 100) +(dezena * 10) + centena
+
+print(f"Seu número invertido é: {total}")
+
+
+
+# Exer 8
+# Escreva um programa em python que entre com a base e altura 
+# de um retângulo e imprimir a seguinte saída:
+# perímetro:
+# área:
+# diagonal:
+
+h = float(input("Digite a altura:"))
+b = float(input("Digite a base: "))
+
+p = (b*2)+(h*2)
+a = b * h
+d = ((b)**2 + (h)**2)**(1/2)
+
+print(f"perimetro: {p} | área: {a} u² | diagonal: {d} u")
+
+
+
+# Exer 9
+# Escreva um programa em python que calcule e imprima a área 
+# de um triângulo
+
+h = float(input("Digite a altura:"))
+b = float(input("Digite a base: "))
+
+a = (b * h)/2
+
+print(f"Sua área é {a} u²")
